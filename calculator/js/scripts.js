e9d54d7c24c0120ddbb25560e695cd31e4826f68
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-01-11 13:00:44 -0800

    It will add subtract, multiply, and divid form and front-side code

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -22,15 +22,44 @@ var divide = function(number1, number2){
 
 $(document).ready(function(event) {
 
+	// Add Calculator
 	$("form#add").submit(function(event){
-
 		event.preventDefault();
 		var number1 = parseInt($("#add1").val());
 		var number2 = parseInt($("#add2").val());
-		var result = add(number1, number2)
+		var result = add(number1, number2);
 		$("#output").text(result);
 	});
+
+	//Subtract Caltuclator
+	$("form#subtract").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var result = subtract(number1, number2);
+		$("#output2").text(result);
+	});
+
+	//Multiply Calculator
+	$("form#multiply").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#multiply1").val());
+		var number2 = parseInt($("#multiply2").val());
+		var result = multiply(number1, number2);
+		document.getElementById('output3').value = result;
+	})
+
+	$("form#divide").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#divide1").val());
+		var number2 = parseInt($("#divide2").val());
+		var result = divide(number1, number2);
+		document.getElementById("output4").value = result;
+	});
+
 });
+
+
 
 
 
